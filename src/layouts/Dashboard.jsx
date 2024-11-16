d4983d1950baf473fb337d6bb20fbc1c7463d274
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2024-11-16 23:44:31 +0600

    update show

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -14,7 +14,7 @@ const Dashboard = () => {
             <div className='bg-[#07332F] text-white py-5'>
                 <div className=' flex items-center justify-between px-12 lg:px-24'>
                     <div>
-                        <h3 className='font-semibold text-xl'>Doc House</h3>
+                        <Link to={"/"} className='font-semibold text-xl'>Doc House</Link>
                     </div>
                     <div className="indicator">
                         <span className="indicator-item badge badge-secondary">99+</span>
@@ -23,60 +23,6 @@ const Dashboard = () => {
                 </div>
             </div>
             <div>
-                {/* <div className="drawer lg:drawer-open">
-                    <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                    <div className="drawer-content relative">
-                        <Outlet></Outlet>
-                        <label htmlFor="my-drawer-2" className=" top-2 left-2 text-3xl fixed drawer-button lg:hidden"><FaBars></FaBars></label>
-
-                    </div>
-                    <div className="drawer-side bg-[#F5E1DA]">
-                        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                        <ul className="menu p-4 w-80  text-base-content uppercase font-semibold text-base">
-                            <div className='text-center font-bold uppercase font-family py-16'>
-                                <p className='text-3xl '>GoalGurus</p>
-                                <p className='  text-xl md:tracking-wider '>Football Academy</p>
-                            </div>
-                            {
-                                isAdmin && <>
-                                    <li>
-                                        <Link to="/dashboard/manageClasses"><FaSquareFull></FaSquareFull>Manage Classes</Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/dashboard/allUsers"><FaUsers></FaUsers>Manage Users</Link>
-                                    </li>
-                                </> || isCoach && <>
-                                    <li>
-                                        <Link to="/dashboard/addClass"><FaArrowAltCircleDown></FaArrowAltCircleDown>Add Class</Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/dashboard/myClasses"><FaWallet></FaWallet>My Classes</Link>
-                                    </li>
-                                </> || <>
-                                    <li>
-                                        <Link to="/dashBoard/myCart"><FaCartArrowDown></FaCartArrowDown> My Selected Classes <span className=" badge badge-secondary">+5</span></Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/dashboard/enrolledClasses"><FaCalendarAlt></FaCalendarAlt>My Enrolled Classes</Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/dashboard/paymentHistory"><FaWallet></FaWallet> Payment History</Link>
-                                    </li>
-                                </>
-                            }
-                            <div className="divider bg-white h-1"></div>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/allClass">Classes</Link>
-                            </li>
-                            <li>
-                                <Link to="/coaches">Coaches</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div> */}
                 <h3 className='text-2xl px-4  lg:px-24 my-4 font-semibold'>Dashboard</h3>
                 <div className="drawer lg:drawer-open  ">
                     <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -90,6 +36,9 @@ const Dashboard = () => {
                         <ul className="menu bg-white text-gray-500 font-semibold  min-h-full w-96 text-xl p-4 lg:px-24">
                             {/* Sidebar content here */}
                             <li>
+                                <Link to={""}>Admin Home</Link>
+                            </li>
+                            <li>
                                 <Link to={"/dashboard/allUsers"}>All Users</Link>
                             </li>
                             <li>
@@ -98,6 +47,7 @@ const Dashboard = () => {
                             <li>
                                 <Link>Manage Doctor</Link>
                             </li>
+                            <h5 className='border border-green-950 my-4'></h5>
                             <li>
                                 <Link to={"/"}>Home</Link>
                             </li>
